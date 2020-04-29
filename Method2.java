f7ce80366a1e13ab9791b88074024a5c3d5d9f05
tanguosheng1999 <55288234+tanguosheng1999@users.noreply.github.com>
2020-04-29 12:33:50 +0800

    Create Method2.java

---
+++ Method2.java
@@ -0,0 +1,13 @@
+```java
+public class Method2 {
+    public static void main(String[] args) {
+        MyInterface myInterface = new MyInterface() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello Method2");
+            }
+        };
+        myInterface.sayHello();
+    }
+}
+```
